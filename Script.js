0d797374f1bb9f1a49e2e0185a80c367734432bb
Rohail Ahmed <ahmedrohail2010@gmail.com>
2023-03-06 21:20:12 +0000

    Webhook Commit

---
+++ Script.js
@@ -0,0 +1,27 @@
+function sendMessage() {
+  // Get the message input element
+  const messageInput = document.getElementById('message-input');
+  
+  // Get the webhook URL
+  const webhookUrl = 'https://discord.com/api/webhooks/1082385030447378453/bKwHtloO3tEChZXtIvFMKe__lg_pd-teZ9JAPcuzOKaDx7BqUZMR76DxNDqs9MncQvTN';
+  
+  // Create the data to send to the webhook
+  const data = { content: messageInput.value };
+  
+  // Send the HTTP request to the webhook URL
+  fetch(webhookUrl, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  })
+  .then(response => {
+    if (response.ok) {
+      console.log('Message sent successfully');
+    } else {
+      console.error('Error sending message:', response.status, response.statusText);
+    }
+  })
+  .catch(error => {
+    console.error('Error sending message:', error);
+  });
+}
