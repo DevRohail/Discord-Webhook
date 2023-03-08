3e457b24de157e37688956a9b76ff77a8bad01a7
Rohail Ahmed <ahmedrohail2010@gmail.com>
2023-03-08 20:40:29 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-require("dotenv").config();
-
-console.log(process.env.WEATHER_API_KEY);
-
 function sendMessage() {
-    const webhookUrl = process.env.DISCORD_WEBHOOK;
+    // Get the message input element
+    const messageInput = document.getElementById('message-input');
+    
+    // Get the webhook URL
+    const webhookUrl = 'https://discord.com/api/webhooks/1082766354328858655/0lRO3S6FfC0Zhoh4xfCF89gR0RyDsbYGSbaBNcI12el14iHUh3uizhLmGbEsgmxng7Ae';
+    
+    // Create the data to send to the webhook
     const data = { content: messageInput.value };
     
+    // Send the HTTP request to the webhook URL
     fetch(webhookUrl, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data)
-})
-}
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    })
+    .then(response => {
+      if (response.ok) {
+        console.log('Message sent successfully');
+      } else {
+        console.error('Error sending message:', response.status, response.statusText);
+      }
+    })
+    .catch(error => {
+      console.error('Error sending message:', error);
+    });
+  }
